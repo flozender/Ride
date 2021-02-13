ffb5b67abd03f0267d027d962d4433884a46b5b6
JethroGibbsV <boyapatikrishna99@gmail.com>
2021-02-13 23:14:08 +0530

    Fixed bug and Added completed in trips response

--- server/controllers/loop.js
+++ server/controllers/loop.js
@@ -3,7 +3,9 @@ const db = require('../db/database.js');
 exports.getTripsForLoop = async (data) => {
   try {
     let query = `SELECT H.capacity, H.origin, H.destination, H.when,
-    H.passengers, U.name, U.email, U.phone
+    H.passengers, U.name, U.email, U.phone,
+    CASE WHEN H.when < current_timestamp THEN 1
+    ELSE 0 END AS completed
     FROM hosts H
     LEFT JOIN users U ON U.username = H.username
     WHERE H.origin = $1 AND H.destination = $2 AND H.capacity > 0
